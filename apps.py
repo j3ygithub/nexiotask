628b3e31a8f694db2e51afee069b85b11b32d584
Jimmy Lin <b00502013@gmail.com>
2021-05-15 05:54:01 +0800

    Update apps.py

--- apps.py
+++ apps.py
@@ -25,15 +25,16 @@ def create_app(app_name):
     Application factories
     https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
     """
-    app = Flask(__name__)
-    if app_name == "development":
-        from_object = DevelopmentConfig
-        app.config.from_object(from_object)
-    elif app_name == "testing":
-        from_object = TestingConfig
-        app.config.from_object(from_object)
-    else:
+    configs = {
+        "development": DevelopmentConfig,
+        "testing": TestingConfig,
+    }
+    try:
+        config = configs[app_name]
+    except KeyError:
         raise ValueError(f'"{app_name}" is not a valid app_name.')
+    app = Flask(__name__)
+    app.config.from_object(config)
     db.init_app(app)
     ma.init_app(app)
     return app
