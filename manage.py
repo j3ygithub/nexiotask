42528969a71f10336a81e01602c67be5069c7131
Jimmy Lin <b00502013@gmail.com>
2021-05-15 23:21:04 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,6 @@
 import argparse
 
-from apps import create_app
-
-
-def helloworld():
-    print("hello world")
+from apps import create_app, db
 
 
 def runserver():
@@ -12,17 +8,29 @@ def runserver():
     app.run()
 
 
-tasks = {
-    "helloworld": helloworld,
-    "runserver": runserver,
-}
-available_task = ", ".join(tasks.keys())
+def db_create_all():
+    app = create_app("development")
+    app.app_context().push()
+    db.create_all()
 
+
+def db_drop_all():
+    app = create_app("development")
+    app.app_context().push()
+    db.drop_all()
+
+
+tasks = {
+    "runserver": runserver,
+    "db_create_all": db_create_all,
+    "db_drop_all": db_drop_all,
+}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "task_name", help=f"Please enter a script name. Choices are: {available_task}."
+        "task_name",
+        help=f'Please enter a script name. Choices are: {", ".join(tasks.keys())}.',
     )
     args = parser.parse_args()
     task_name = args.task_name
